9168a8065089ca74162e1b8f0daecf151f871727
GowthamiBannur <gowthamibputtur@gmail.com>
2026-02-26 14:57:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -111,7 +111,8 @@ with tab1:
                - Prioritization for overloaded sprints.
                - Narrative for PM transition.
             4. DOUBTS: Queries for Dev/PO.
-            
+           
+            """
             res = client.chat.completions.create(model="llama-3.3-70b-versatile", messages=[{"role": "user", "content": prompt}]).choices[0].message.content
             st.session_state.audit_report = res
             
